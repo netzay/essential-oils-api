9eb434b0a28ee4ee2d5c02c7e318af76175e38d8
netzay <netzelally@gmail.com>
2019-08-22 11:00:31 -0600

    update nokogiri ~> 1.10.4.

---
+++ app/controllers/api/favorites_controller.rb
@@ -0,0 +1,20 @@
+class FavoritesController < ApplicationController
+  def index
+    render :json => Favorite.all
+  end
+
+  def create
+    @favorite = Favorite.find_or_create_by(
+      oil: params["id"],
+    )
+    render json: @favorite
+  end
+
+  def destroy
+    @favorite = Favorite.find(params[:id])
+    @favorite.destroy
+    render json: @favorite
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   namespace :api do
     resources :oils, except: [:new, :edit]
   end
+    resources :favorites, only: [:index, :create, :destroy]
 end
